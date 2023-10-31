e9d5b383b1699b57ddc6dff984b717296a48c42e
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 20:21:42 -0300

    info about requestScope inside Singleton

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -13,6 +13,21 @@ public class LoginController {
 	
 	private final LoginProcessor lp;  // si bien es final, atento que es un request-scope bean!
 	
+	/**
+	 * If this controller's constructor is injected with a @RequestScope bean, it's important to note that the actual 
+	 * creation of the @RequestScope bean doesn't happen at the time the @Controller is created. Instead, Spring will 
+	 * inject a proxy or placeholder for the @RequestScope bean into your controller.
+	 * 
+	 * When a request comes in, Spring will create a new instance of the @RequestScope bean and associate it with the 
+	 * current request. This is the moment when the actual @RequestScope bean is created and initialized.
+	 * 
+	 * The proxy or placeholder in your controller will point to the correct instance of the @RequestScope bean for 
+	 * that specific request.
+	 * 
+	 * Once the request is processed, the request-scoped bean associated with that request is discarded. The proxy or 
+	 * placeholder in your controller will then point to a new instance for the next request.
+	 */
+	
 	public LoginController(LoginProcessor lp) {
 		this.lp = lp;
 	}
