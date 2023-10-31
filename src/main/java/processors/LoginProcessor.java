528ff84dc9093c0572489e47f99c2bf06592ddcd
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 19:38:15 -0300

    modif login processor

--- src/main/java/processors/LoginProcessor.java
+++ src/main/java/processors/LoginProcessor.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import services.LoggedUserManagementService;
+import services.LoginCountService;
 
 @Component
 @RequestScope
@@ -13,13 +14,18 @@ public class LoginProcessor {
 	private String password;
 	
 	private final LoggedUserManagementService loggedUserManagementService;
+	private final LoginCountService loginCountService;
 	
 	// autowired DI
-	public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+	public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+			LoginCountService loginCountService) {
 		this.loggedUserManagementService = loggedUserManagementService;
+		this.loginCountService = loginCountService;
 	}
 	
 	public boolean login() {
+		loginCountService.increment();
+		
 		String username = this.getUsername();
 		String password = this.getPassword();
 		
