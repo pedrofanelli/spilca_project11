386bc658084c9216686bd9596f2b9275b74b84c2
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-31 19:35:51 -0300

    loginCountService

---
+++ src/main/java/services/LoginCountService.java
@@ -0,0 +1,19 @@
+package services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Service
+@ApplicationScope
+public class LoginCountService {
+
+	private int count;
+	
+	public void increment() {
+		this.count++;
+	}
+	
+	public int getCount() {
+		return this.count;
+	}
+}
